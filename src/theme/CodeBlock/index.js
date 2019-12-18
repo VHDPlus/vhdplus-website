f2a74ed9386df234db0335edf1f26aadaeae4cc2
Hendrik Mennen <hendrik.mennen@hs-osnabrueck.de>
2019-12-18 23:22:43 +0100

    prism highlight

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -14,6 +14,71 @@ import rangeParser from 'parse-numeric-range';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './styles.module.css';
 
+// Custom Language
+import Prism from 'prism-react-renderer/prism';
+(typeof global !== 'undefined' ? global : window).Prism = Prism;
+
+Prism.languages.vhdp = {
+  'alternativecomment': [
+		{
+			pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
+			lookbehind: true
+		},
+		{
+			pattern: /(^|[^\\:])\/\/.*/,
+			lookbehind: true,
+			greedy: true
+		}
+	],
+  'comment': /--.+/,
+	// support for all logic vectors
+	'vhdl-vectors': {
+		'pattern': /\b[oxb]"[\da-f_]+"|"[01uxzwlh-]+"/i,
+		'alias': 'number'
+	},
+	// support for operator overloading included
+	'quoted-function': {
+		pattern: /"\S+?"(?=\()/,
+		alias: 'function'
+	},
+	'string': /"(?:[^\\"\r\n]|\\(?:\r\n|[\s\S]))*"/,
+  'constant': /\b(?:use|library)\b/i,
+  
+  'keyword': /\b(std_logic_vector|std_ulogic_vector|std_logic|bit_vector|bit|boolean|natural|positive|integer|real|character|string|unsigned|signed|clk|rising_edge|falling_edge)(?![-\S])?\b/ig,
+  'loopkeyword': /\b(main|class|component|thread|attributedeclaration|newfunction|newcomponent|functionparameter|functioncontent|vhdl|entity|is|library|use|of|architecture|component|generic|port|map|end|name|out|inout|buffer|generate|input|process|begin|to|downto|in|out|with|select|signal|constant|variable|range|others|rise|fall|type|array|package|function|stepfunction|connections|include|stepfor|\'length|to_integer|abs|resize|to_bit|to_bitvector|to_stdlogicvector|to_unsigned|to_signed)(?![-\S])?\b/ig,
+  'controlflow': /\b(step|stepif|stepelse|stepelsif|stepcase|stepwhen|while|do|wait|case|when|else|elsif|if|then|for|loop|break|exit|default|return)(?![-\S])?\b/ig,
+  'boolean': /\b(?:true|false)\b/i,
+	'function': /\w+(?=\()/,
+	// decimal, based, physical, and exponential numbers supported
+	'number': /'[01uxzwlh-]'|\b(?:\d+#[\da-f_.]+#|\d[\d_.]*)(?:e[-+]?\d+)?/i,
+	'operator': /\b(?:abs|not|mod|rem|sll|srl|sla|sra|rol|ror|and|or|nand|xnor|xor|nor)\b/i,
+	'punctuation': /[{}[]/
+};
+
+Prism.languages.vhdl = {
+	'comment': /--.+/,
+	// support for all logic vectors
+	'vhdl-vectors': {
+		'pattern': /\b[oxb]"[\da-f_]+"|"[01uxzwlh-]+"/i,
+		'alias': 'number'
+	},
+	// support for operator overloading included
+	'quoted-function': {
+		pattern: /"\S+?"(?=\()/,
+		alias: 'function'
+	},
+	'string': /"(?:[^\\"\r\n]|\\(?:\r\n|[\s\S]))*"/,
+	'constant': /\b(?:use|library)\b/i,
+	// support for predefined attributes included
+	'keyword': /\b(?:'active|'ascending|'base|'delayed|'driving|'driving_value|'event|'high|'image|'instance_name|'last_active|'last_event|'last_value|'left|'leftof|'length|'low|'path_name|'pos|'pred|'quiet|'range|'reverse_range|'right|'rightof|'simple_name|'stable|'succ|'transaction|'val|'value|access|after|alias|all|architecture|array|assert|attribute|begin|block|body|buffer|bus|case|component|configuration|constant|disconnect|downto|else|elsif|end|entity|exit|file|for|function|generate|generic|group|guarded|if|impure|in|inertial|inout|is|label|library|linkage|literal|loop|map|new|next|null|of|on|open|others|out|package|port|postponed|procedure|process|pure|range|record|register|reject|report|return|select|severity|shared|signal|subtype|then|to|transport|type|unaffected|units|until|use|variable|wait|when|while|with)\b/i,
+	'boolean': /\b(?:true|false)\b/i,
+	'function': /\w+(?=\()/,
+	// decimal, based, physical, and exponential numbers supported
+	'number': /'[01uxzwlh-]'|\b(?:\d+#[\da-f_.]+#|\d[\d_.]*)(?:e[-+]?\d+)?/i,
+	'operator': /[<>]=?|:=|[-+*/&=]|\b(?:abs|not|mod|rem|sll|srl|sla|sra|rol|ror|and|or|nand|xnor|xor|nor)\b/i,
+	'punctuation': /[{}[\];(),.:]/
+};
+
 const highlightLinesRangeRegex = /{([\d,-]+)}/;
 
 export default ({children, className: languageClassName, metastring}) => {
