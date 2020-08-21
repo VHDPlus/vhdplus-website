405b6785722cc8e4c1ace2f22232ae11c6bec1f0
Leon Beier <lbeier@vhdplus.com>
2020-08-21 19:22:24 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,9 +17,9 @@ import Slider from "react-slick";
 
 const features = [
   {
-    title: <>Unlimited Threads</>,
+    title: <>Maximum Performance</>,
     imageUrl: "img/icons/cpu_icon.svg",
-    description: <>Use Maximum Performance for Every Task</>
+    description: <>Every Thread like a New Processor and Same Performance as Low-Level Languages</>
   },
   {
     title: <>Built-in libraries</>,
