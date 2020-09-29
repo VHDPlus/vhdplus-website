fc555dad56574af26c543cf4a01f23d5bdcf4c83
Hendrik Mennen <hmennen@vhdplus.com>
2020-09-29 14:26:14 +0200

    .

--- src/pages/support.js
+++ src/pages/support.js
@@ -3,7 +3,7 @@ import Layout from '@theme/Layout';
 import {Redirect} from '@docusaurus/router';
 
 function Hello() {
-  return <Redirect to="/docs/community/overview#found-an-issue-in-our-ide" />;
+  return <Redirect to="/docs/community/overview#need-help-with-vhdp" />;
 }
 
 export default Hello;
