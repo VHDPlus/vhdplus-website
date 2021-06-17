d2ccd895930dc775b6d1a20c447976eed13324a0
Hendrik Mennen <hmennen@vhdplus.com>
2021-06-17 12:20:21 +0200

    download

---
+++ src/pages/download.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import {Redirect} from '@docusaurus/router';
+
+function Hello() {
+  return <Redirect to="/docs/getstarted#install-vhdplus-ide" />;
+}
+
+export default Hello;
