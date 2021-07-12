aae996b6c374b30c20e5e4438540e9e4246ba4cb
Hendrik Mennen <hmennen@vhdplus.com>
2021-07-12 23:58:51 +0200

    improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import styles from "../css/index.module.css";
 import Particles from "react-particles-js";
 import CustomCodeBlock from '../components/CustomCodeBlock'
 import Slider from "react-slick";
+import { Container, Row, Col } from 'react-bootstrap';
 import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 
 const snippetVHDP = {
@@ -119,7 +120,7 @@ const features = [
 const sliders = [
   {
     title: "Code Assistant",
-    videoUrl: "/img/slides/VHDP.png",
+    imageSrc: "/img/slides/VHDP.png",
     description: (
       <>
         VHDPlus IDE makes writing code as easy as possible. Multiple Features
@@ -130,7 +131,7 @@ const sliders = [
   },
   {
     title: "Seamless Quartus Integration",
-    videoUrl: "/img/slides/Connection.jpg",
+    imageSrc: "/img/slides/Connection.jpg",
     description: (
       <>
         Connect pins, compile and program directly from our IDE. All important
@@ -141,7 +142,7 @@ const sliders = [
   },
   {
     title: "Simulation Assistant",
-    videoUrl: "/img/slides/Simulation.png",
+    imageSrc: "/img/slides/Simulation.png",
     description: (
       <>
         If you ever tried programming FPGAs you probably noticed that the HDL
@@ -153,7 +154,7 @@ const sliders = [
   ,
   {
     title: "Software Support",
-    videoUrl: "/img/slides/Debugging.png",
+    imageSrc: "/img/slides/Debugging.png",
     description: (
       <>
         For some applications using an FPGA can have disadvantages. In this case you can use the NIOS II Softcore processor and program it like an Arduino.
@@ -228,7 +229,7 @@ class Home extends React.Component {
     }
   }
 
-  
+
 
   render() {
     var settings = {
@@ -304,8 +305,8 @@ class Home extends React.Component {
         />
 
         <header className={classnames("hero", styles.heroBanner)}>
-          <div className="container">
-            <div className={styles.heroLogo}>
+          <Container>
+          <div className={styles.heroLogo}>
               <img
                 alt="VHDPlus Logo"
                 src={"img/vhdp.svg"}
@@ -318,58 +319,53 @@ class Home extends React.Component {
               <p className="hero__subtitle">The FPGA Programming Revolution</p>
               <div className={styles.buttons}>
                 <Link
-                  className={classnames("button button--primary button--lg hideMobile",styles.getStarted)}
+                  className={classnames("button button--primary button--lg", styles.getStarted)}
                   to={"docs/getstarted#install-vhdplus-ide"}>
                   DOWNLOAD
                 </Link>
                 <Link
-                  className={classnames("button button--secondary button--outline button--lg text-secondary",styles.getStartedAlternative, styles.getStarted)}
+                  className={classnames("button button--secondary button--outline button--lg", styles.getStartedAlternative, styles.getStarted)}
                   to={"docs/getstarted"}>
                   GET STARTED
                 </Link>
               </div>
             </div>
-          </div>
+          </Container>
         </header>
 
         <main>
           {features && features.length && (
-            <section className={styles.features}>
-              <div className="container padding-top--lg">
-                <div className="row padding-top--lg">
-                  {features.map(({ imageUrl, title, description }, idx) => (
-                    <div
-                      key={idx}
-                      className={classnames("col col--4", styles.feature)}
-                    >
-                      {imageUrl && (
-                        <div className="text--center">
-                          <img
-                            className={styles.featureImage}
-                            src={imageUrl}
-                            alt={title}
-                          />
-                          <h3 style={{ padding: "10px" }}>{title}</h3>
-                          <p>{description}</p>
-                        </div>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </section>
+            <Container>
+              <Row className="padding-vert--xl">
+                {features.map(({ imageUrl, title, description }, idx) => (
+                  <Col key={idx}>
+                    {imageUrl && (
+                      <div className="text--center">
+                        <img
+                          className={styles.featureImage}
+                          src={imageUrl}
+                          alt={title}
+                        />
+                        <h3 className="p-2">{title}</h3>
+                        <p>{description}</p>
+                      </div>
+                    )}
+                  </Col>
+                ))}
+              </Row>
+            </Container>
           )}
           {sliders && sliders.length && (
-            <div className="container">
+            <Container>
               <Slider
                 id="SlickSlide"
                 className="roundcorner"
                 ref={slider => (this.slickRef = slider)}
                 {...settings}
               >
-                {sliders.map(({ videoUrl, title, description }, idx) => (
+                {sliders.map(({ imageSrc, title, description }, idx) => (
                   <div key={idx}>
-                    <img src={videoUrl} className="roundcorner"></img>
+                    <img src={imageSrc} className="roundcorner"></img>
                     <div className={classnames("hideDesktop", styles.slidecaption)}>
                       <h3>{title}</h3>
                       {description}
@@ -377,25 +373,24 @@ class Home extends React.Component {
                   </div>
                 ))}
               </Slider>
-
-              <div className="row padding-vert--lg padding-horiz--md hideMobile">
+              <Row className="margin-vert--xl hideMobile">
                 {sliders.map(({ title, description }, idx) => (
-                  <div
+                  <Col
                     key={idx}
                     className={classnames("col", "padding-vert--lg", styles.slidebutton)}
                     id={"slide" + idx}>
                     <h3>{title}</h3>
                     <span>{description}</span>
-                  </div>
+                  </Col>
                 ))}
-              </div>
-            </div>
+              </Row>
+            </Container>
           )}
 
           <div className={styles.altcolor}>
-            <div className="container padding-vert--lg bottomsplit">
-              <div className="row padding-vert--lg">
-                <div className="col padding--lg coltext">
+            <Container>
+              <Row className="bottomsplit padding-vert--lg">
+                <Col className="padding-vert--md mobileorder-1">
                   <h1>What is an FPGA?</h1>
                   FPGAs complete operations at astonishing rates. They are not
                   limited by thread count, because you program the hardware.
@@ -407,29 +402,21 @@ class Home extends React.Component {
                   environment. But due to the high price and difficulty of
                   programming for private users, FPGAs have not yet been
                   established. This is why we started this project.
-                </div>
-                <div className="col padding-horiz--lg colimage">
-                  <a href="/docs/community/overview">
-                    <img
-                      src="/img/About_VHDPlus.png"
-                      style={{ verticalAlign: "middle" }}
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="container padding-vert--lg">
-              <div className="row padding-vert--lg ">
-                <div className="col padding-horiz--lg coltext">
-                    <CustomCodeBlock header='Blink example code' snippets={[snippetVHDP, snippetVHDL, snippetVerilog]}/>
-                </div>
-                <div className="col padding-horiz--lg colimage">
+                </Col>
+                <Col className="padding-vert--md mobileorder-0">
+                  <img src="/img/About_VHDPlus.png" />
+                </Col>
+              </Row>
+              <Row className="padding-vert--lg">
+                <Col className="mobileorder-1 padding-vert--md">
+                  <CustomCodeBlock header='Blink' snippets={[snippetVHDP, snippetVHDL, snippetVerilog]} />
+                </Col>
+                <Col className="mobileorder-0 padding-vert--md">
                   <h2>VHDP Programming Language</h2>
                   By automatically creating state machines, including
                   synthesizable loops, delays, and functions, and due to a much
                   simpler syntax, you can experience up to 95% code reduction at
-                  100% of the performance! 
+                  100% of the performance!
                   <br />
                   <br />
                   VHDP is not a completely different language, but it extends
@@ -444,36 +431,30 @@ class Home extends React.Component {
                     <li><strong>Simple Syntax ✔</strong></li>
                     <li><strong>Full FPGA Performance ✔</strong></li>
                   </ul>
-                  <br />
-                  <div
-                    className={styles.buttons}
-                    style={{ justifyContent: "left" }}
-                  >
-                    <Link
-                      className="button button--outline button--md"
-                      to={"docs/getstarted/comparison"}
-                    >
-                      Comparison between VHDP with VHDL
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
+                  <Link
+                    className="customLink"
+                    to={"docs/getstarted/comparison"}>
+                    🡒 Comparison between VHDP with VHDL
+                  </Link>
+                </Col>
+              </Row>
+            </Container>
           </div>
-          <div className="container bottomsplit padding-vert--lg">
-            <div className="row padding-vert--lg">
-              <div className="col padding--lg coltext">
-                <h1>Our Hardware</h1>
-                Professional grade hardware designed for inexperienced users and
-                professionals. One board for all your projects, faster than
-                processors and the perfect companion for the VHDPlus IDE.
-                <br/>
-                <br/>
+
+          <Container>
+            <Row className="bottomsplit padding-vert--lg">
+              <Col className="padding-vert--md mobileorder-1">
+                <h2>High Speed Hardware</h2>
+                <p>
+                  Professional grade hardware designed for inexperienced users and
+                  professionals. One board for all your projects, faster than
+                  processors and the perfect companion for the VHDPlus IDE.
+                </p>
                 <ul>
-                    <li><strong>VHDPlus IDE Integration ✔</strong></li>
-                    <li><strong>Plug&Play experience ✔</strong></li>
-                    <li><strong>NIOS II Support ✔</strong></li>
-                  </ul>
+                  <li><strong>VHDPlus IDE Integration ✔</strong></li>
+                  <li><strong>On-Board Programmer ✔</strong></li>
+                  <li><strong>Plug&Play experience ✔</strong></li>
+                </ul>
                 <Link
                   className="button button--outline button--md"
                   to={"docs/components/overview"}>
@@ -484,52 +465,41 @@ class Home extends React.Component {
                   to={"https://shop.vhdplus.com"}>
                   Visit our Store
                 </Link>
-              </div>
-              <div className="col padding-horiz--lg colimage">
-                <div className="vcenter"></div>
-                <video className="playOnHover" muted loop autoPlay><source src="/img/vhdpshield/Shield.webm" type="video/mp4"/>Your browser does not support the video tag. You can download the video anyway.</video>       
-              </div>
-            </div>
-          </div>
-          <div className="container padding-vert--lg">
-            <div className="row padding-vert--lg">
-              <div className="col padding--lg colimage">
-                <div className="vcenter"></div>
-                <img
-                    src="/img/US_Connect.png"
-                    style={{ verticalAlign: "middle" }}
-                  />
-              </div>
-              <div className="col padding-horiz--lg coltext">
+              </Col>
+              <Col className="padding-vert--md mobileorder-0">
+                <video muted loop autoPlay><source src="/img/vhdpshield/Shield.webm" type="video/mp4" />Your browser does not support the video tag. You can download the video anyway.</video>
+              </Col>
+            </Row>
+            <Row className="padding-vert--lg">
+              <Col className="padding-vert--md">
+                <img src="/img/US_Connect.png" />
+              </Col>
+              <Col className="padding-vert--md">
                 <h2>Easy to learn</h2>
-                Interested but don't know how to start? Check out our
-                documentation and example projects! Learn how to program your
-                robot and make it yours. Do projects you may not have thought of
-                while working with microcontrollers!<br></br>
-                <br></br>
-                <br></br>
-                <div
-                  className={styles.buttons}
-                  style={{ justifyContent: "left" }}>
-                  <Link
-                      className="button button--outline button--md"
-                      to={"docs/getstarted/vhdp"}>
-                      Documentation
-                    </Link>
-                    <Link
-                      className="button button--outline button--md margin-left--sm"
-                      to={"docs/community/overview"}>
-                      Example Projects
-                    </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+                <p>
+                  Interested but don't know how to start? Check out our
+                  documentation and example projects! Learn how to program your
+                  robot and make it yours. Do projects you may not have thought of
+                  while working with microcontrollers!
+                </p>
+                <Link
+                  className="button button--outline button--md"
+                  to={"docs/getstarted/vhdp"}>
+                  Documentation
+                </Link>
+                <Link
+                  className="button button--outline button--md margin-left--sm"
+                  to={"docs/community/overview"}>
+                  Example Projects
+                </Link>
+              </Col>
+            </Row>
+          </Container>
 
           <div className={classnames(styles.altcolor, styles.altcolorend)}>
-            <div className="container padding-vert--lg">
-              <div className="row padding-vert--lg">
-                <div className="col padding--lg coltext">
+            <Container>
+              <Row className="padding-vert--lg">
+                <Col className="padding-vert--md mobileorder-1">
                   <h2>Video Tutorials</h2>
                   You don't like to read? Then try our video tutorials in which
                   you can learn all important features of our IDE. From simple
@@ -544,25 +514,22 @@ class Home extends React.Component {
                     className="button button--outline button--lg margin-vert--sm"
                     href="https://www.youtube.com/channel/UC7qiOvlaBSiWyAb7R1xTaEw"
                   >
-                    <div style={{display:"flex", alignItems:"center"}}>
-                    <span style={{}}>YouTube Channel</span>               
-                    <svg style={{marginLeft: "10px"}} xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="red" viewBox="0 0 16 16">
-                      <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
-                    </svg>
-                    </div>  
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <span style={{}}>YouTube Channel</span>
+                      <svg style={{ marginLeft: "10px" }} xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="red" viewBox="0 0 16 16">
+                        <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
+                      </svg>
+                    </div>
                   </a>
-                </div>
-                <div className="col padding-horiz--lg colimage">
-                  <div className="vcenter"></div>
-                  <a href="https://www.youtube.com/channel/UC7qiOvlaBSiWyAb7R1xTaEw">
-                    <img
-                      src="/img/Youtube.webp"
-                      style={{ verticalAlign: "middle" }}
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
+                </Col>
+                <Col className="padding-vert--md mobileorder-0">
+                  <img
+                    src="/img/Youtube.webp"
+                    style={{ verticalAlign: "middle" }}
+                  />
+                </Col>
+              </Row>
+            </Container>
           </div>
         </main>
       </Layout>
