da716a81dfe89109b34674f377d995d97be63ed9
Hendrik Mennen <hmennen@vhdplus.com>
2021-07-12 11:49:30 +0200

    .

--- src/components/CustomCodeBlock.tsx
+++ src/components/CustomCodeBlock.tsx
@@ -2,23 +2,25 @@ import React, { useState } from 'react'
 import CodeBlock from '@theme/CodeBlock'
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
+import classnames from "classnames";
+
+import ccb from "../css/customcodeblock.module.css";
 
 export default function CustomCodeBlock({
   snippets,
 }) {
 
-  let wrapperClass = ''
-
   return (
     <>
-      <div className={wrapperClass}>
+      <div className={ccb.wrap}>
         <>
         <Tabs
+            className={classnames("codeblocktabs",ccb.tabs)}
             defaultValue={snippets[0].header}
             values={snippets.map(snip => ({label: snip.header, value: snip.header}))}>
           {snippets.map(({ language, code, header, highlight }, idx) => (
-                <TabItem value={header}>
-                    <CodeBlock className={language} metastring={highlight}>
+                <TabItem value={header} className={ccb.tabitem}>
+                    <CodeBlock className={classnames(language,ccb.codeblock)} metastring={highlight}>
                     {code}
                     </CodeBlock>
                 </TabItem>
